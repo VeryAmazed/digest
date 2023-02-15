43bca4c34c548c56afcc15f372fe7546b8c04776
VeryAmazed <alan.h.zheng@outlook.com>
2023-02-15 00:51:47 -0500

    added trivial Catch2 compatibility into cmake test file

--- test.cpp
+++ test.cpp
@@ -1,9 +1,19 @@
 #include "nthash.hpp"
+#include <catch2/catch_test_macros.hpp>
 
+/*
 int main(){
 	std::string str = "TGACGCATTTGGCGAGATGATG";
 	nthash::NtHash* test_hash = new nthash::NtHash(str, 1, 4);
 	std::cout << test_hash->roll() << std::endl;
 	
 	return 0;
+}
+*/
+
+TEST_CASE("Testing: ntHash can roll()"){
+	std::string str = "TGACGCATTTGGCGAGATGATG";
+	nthash::NtHash* test_hash = new nthash::NtHash(str, 1, 4);
+	REQUIRE(test_hash->roll() == 1);
+	REQUIRE(test_hash->roll() == 0);
 }
