ee22f4ba3dfc26f86810dfbc0bf36571f2427690
VeryAmazed <alan.h.zheng@outlook.com>
2023-11-08 11:52:26 -0500

    added in some append_seq sanity check benchmarks. Still need to put pauses around some timings of calls that aren't relevant

--- tests/benchmark.cpp
+++ tests/benchmark.cpp
@@ -14,6 +14,9 @@
 
 std::vector<std::string> bench_strs;
 std::string s;
+std::string s1;
+std::string s2;
+
 
 void setupStrings(){
 	std::string files[] = {
@@ -37,6 +40,10 @@ static void ecoli(const benchmark::State& state) {
 	s = bench_strs[1].substr(0, state.range(0));
 }
 
+static void random_append_seq(const benchmark::State& state){
+	s1 = bench_strs[0].substr(0, state.range(0));
+	s2 = s = bench_strs[0].substr(state.range(0), state.range(1));
+}
 
 // static void BM_NtHashConstruction(benchmark::State& state){
 // 	for(auto _ : state) {
@@ -57,7 +64,7 @@ static void BM_NtHashRoll(benchmark::State& state) {
 	}
 	state.SetComplexityN(state.range(0));
 }
-BENCHMARK(BM_NtHashRoll)->Range(1<<6, 1<<18)->Setup(random)->Complexity();
+// BENCHMARK(BM_NtHashRoll)->Range(1<<6, 1<<18)->Setup(random)->Complexity();
 
 
 // static void BM_ModMinConstruction(benchmark::State& state){
@@ -82,7 +89,7 @@ static void BM_ModMinRoll(benchmark::State& state) {
 	}
 	state.SetComplexityN(state.range(0));
 }
-BENCHMARK(BM_ModMinRoll)->Range(1<<6, 1<<18)->Setup(random)->Complexity();
+// BENCHMARK(BM_ModMinRoll)->Range(1<<6, 1<<18)->Setup(random)->Complexity();
 
 
 // static void BM_WindowMinConstructionFixWind(benchmark::State& state){
@@ -107,7 +114,7 @@ static void BM_WindowMinRollFixWind(benchmark::State& state){
     }
 	state.SetComplexityN(state.range(0));
 }
-BENCHMARK(BM_WindowMinRollFixWind)->Range(1<<6, 1<<18)->Setup(random)->Complexity();
+// BENCHMARK(BM_WindowMinRollFixWind)->Range(1<<6, 1<<18)->Setup(random)->Complexity();
 
 
 
@@ -134,7 +141,7 @@ static void BM_WindowMinRollFixLen(benchmark::State& state){
     }
 	state.SetComplexityN(state.range(0));
 }
-BENCHMARK(BM_WindowMinRollFixLen)->Range(1<<6, 1<<18)->Setup(random)->Complexity();
+// BENCHMARK(BM_WindowMinRollFixLen)->Range(1<<6, 1<<18)->Setup(random)->Complexity();
 
 
 
@@ -162,10 +169,7 @@ static void BM_SyncmerRollFixWind(benchmark::State& state){
     }
 	state.SetComplexityN(state.range(0));
 }
-BENCHMARK(BM_SyncmerRollFixWind)->Range(1<<6, 1<<18)->Setup(random)->Complexity();
-
-
-
+// BENCHMARK(BM_SyncmerRollFixWind)->Range(1<<6, 1<<18)->Setup(random)->Complexity();
 
 
 // static void BM_SyncmerConstructionFixLen(benchmark::State& state){
@@ -191,10 +195,24 @@ static void BM_SyncmerRollFixLen(benchmark::State& state){
     }
 	state.SetComplexityN(state.range(0));
 }
-BENCHMARK(BM_SyncmerRollFixLen)->Range(1<<6, 1<<18)->Setup(random)->Complexity();
+// BENCHMARK(BM_SyncmerRollFixLen)->Range(1<<6, 1<<18)->Setup(random)->Complexity();
 
+// more of a sanity check
+static void BM_append_seq(benchmark::State& state){
+	int kmer_len = 64;
+	for(auto _ : state) {
+		digest::ModMin dig(s1, kmer_len, 17);
+		dig.append_seq(s2);
+		std::vector<size_t> vec;
+		vec.reserve(state.range(0) + state.range(1));
 
-
+		benchmark::DoNotOptimize(vec);
+		dig.roll_minimizer(state.range(0), vec);
+		benchmark::ClobberMemory();
+	}
+}
+BENCHMARK(BM_ModMinRoll)->Arg(127)->Setup(random);
+BENCHMARK(BM_append_seq)->Args({63, 64})->Setup(random_append_seq);
 
 int main(int argc, char** argv)
 {
