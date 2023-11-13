1bb1c05a58bc7c82eeab5f31fcd49139245610db
ishmeal <me@ishme.al>
2023-11-13 14:07:45 -0500

    optimizations

--- src/mod_minimizer.cpp
+++ src/mod_minimizer.cpp
@@ -3,21 +3,29 @@
 namespace digest{
     void ModMin::roll_minimizer(unsigned amount, std::vector<size_t>& vec){
         if(!is_valid_hash) return;
-        do{
-            if(get_minimized_h() == 0){
+
+		if(get_minimized_h() == 0) {
+			do {
                 if(chash % mod == congruence){
                     vec.push_back(get_pos());
-                } 
-            }else if(get_minimized_h() == 1){
+                }
+			} while(roll_one() && vec.size() < amount);
+			return;
+		}
+
+        if(get_minimized_h() == 1) {
+			do {
                 if(fhash % mod == congruence){
                     vec.push_back(get_pos());
                 }
-            }else{
-                if(rhash % mod == congruence){
-                    vec.push_back(get_pos());
-                }
-            }
-        }while(roll_one() && vec.size() < amount);
+			} while(roll_one() && vec.size() < amount);
+			return;
+		}
 
+		do {
+			if(rhash % mod == congruence){
+				vec.push_back(get_pos());
+			}
+        } while(roll_one() && vec.size() < amount);
     }
 }
