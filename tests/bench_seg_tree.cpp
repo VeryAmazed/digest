9ed784276ce829b339c5d8155e9dba49f6a1c1e9
ishmeal <me@ishme.al>
2023-11-26 16:05:35 -0500

    testing monoqueue

--- tests/bench_seg_tree.cpp
+++ tests/bench_seg_tree.cpp
@@ -1,4 +1,5 @@
 #include <benchmark/benchmark.h>
+#include <cwchar>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -48,6 +49,8 @@ std::vector<int> inputs;
 std::vector<int> st_outputs;
 std::vector<int> mset_outputs;
 std::vector<int> naive_outputs;
+std::vector<int> monoqueue_outputs;
+std::vector<int> monoqueue_outputs2;
 
 void setupInput(){
     std::string path = "../tests/benchmark_seg_tree_input/nums.txt";
@@ -66,6 +69,8 @@ static void BM_SegTree(benchmark::State& state){
         st_outputs.clear();
         st_outputs.reserve(INPUT_SIZE);
 		int k = state.range(0);
+        state.ResumeTiming();
+
         int ind = 0;
         MinSegmentTree<int> st(k);
         while(ind < k){
@@ -73,7 +78,6 @@ static void BM_SegTree(benchmark::State& state){
             ind++;
         }
         int kick = 0;
-        state.ResumeTiming();
         while(ind < INPUT_SIZE){
             st_outputs.push_back(st.segtree[1]);
             st.set(kick, inputs[ind]);
@@ -84,6 +88,7 @@ static void BM_SegTree(benchmark::State& state){
             //kick = (kick+1)%k;
             ind++;
         }
+		st_outputs.push_back(st.segtree[1]);
 	}
 }
 BENCHMARK(BM_SegTree)->RangeMultiplier(2)->Range(1<<2, 1<<6);
@@ -111,9 +116,10 @@ static void BM_mset(benchmark::State& state){
             if(kick == k) kick = 0;
             ind++;
         }
+		mset_outputs.push_back(*(mset.begin()));
 	}
 }
-BENCHMARK(BM_mset)->RangeMultiplier(2)->Range(1<<2, 1<<6);
+//BENCHMARK(BM_mset)->RangeMultiplier(2)->Range(1<<2, 1<<6);
 
 static void BM_naive(benchmark::State& state){
     for(auto _ : state) {
@@ -123,7 +129,7 @@ static void BM_naive(benchmark::State& state){
 		int k = state.range(0);
 
         state.ResumeTiming();
-        for(int i = 0; i+k < INPUT_SIZE; i++){
+        for(int i = 0; i+k <= INPUT_SIZE; i++){
             int minAm = inputs[i];
             for(int j = i+1; j < i + k; j++){
                 minAm = std::min(minAm, inputs[j]);
@@ -134,6 +140,174 @@ static void BM_naive(benchmark::State& state){
 }
 BENCHMARK(BM_naive)->RangeMultiplier(2)->Range(1<<2, 1<<6);
 
+
+
+
+
+
+
+
+
+class MonoQueue {
+	std::pair<int,int> deq[64];
+	int head = 0, tail = 0;
+	const int k;
+
+	// cannot call this while full
+	bool empty() {
+		return head == tail;
+	}
+
+	void emplace_back(int l, int r) {
+		deq[tail] = {l, r};
+		if (tail == k-1) tail = 0;
+		else tail++;
+	}
+
+	void pop_front() {
+		if (head == k-1) head = 0;
+		else head++;
+	}
+
+	void pop_back() {
+		if (--tail == -1) tail = k - 1;
+	}
+
+	int front1() {
+		return deq[head].first;
+	}
+
+	int back() {
+		return deq[tail == 0 ? k - 1 : tail - 1].second;
+	}
+public:
+
+	MonoQueue(int k) : k(k) {}
+
+	void add (int i) {
+		while (!empty() && back() >= inputs[i]) {
+			pop_back();
+		}
+		if (!empty() && front1() == i - k) {
+			pop_front();
+		}
+		emplace_back(i, inputs[i]);
+	}
+
+	int min() {
+		return deq[head].second;
+	}
+};
+
+
+
+
+
+
+
+class MonoQueue2 {
+	std::pair<int,int> deq[64];
+	int head = 0, tail = 0, k;
+
+	bool empty() {
+		return head == tail;
+	}
+
+	void emplace_back(int l, int r) {
+		deq[tail] = {l, r};
+		if (++tail == k) tail = 0;
+	}
+
+	void pop_front() {
+		if (++head == k) head = 0;
+	}
+
+	void pop_back() {
+		if (--tail == -1) tail = k - 1;
+	}
+
+	int front1() {
+		return deq[head].first;
+	}
+
+	int back() {
+		return deq[tail == 0 ? k - 1 : tail - 1].second;
+	}
+
+public:
+	MonoQueue2(int k) : k(k) {}
+
+	void add (int i) {
+		while (!empty() and back() >= inputs[i]) {
+			pop_back();
+		}
+		if (!empty() and front1() == i - k) {
+			pop_front();
+		}
+		emplace_back(i, inputs[i]);
+	}
+
+	int min() {
+		return deq[head].second;
+	}
+};
+
+
+
+
+
+
+
+static void BM_monoqueue(benchmark::State& state) {
+    for(auto _ : state) {
+        state.PauseTiming();
+        monoqueue_outputs.clear();
+		int k = state.range(0);
+        state.ResumeTiming();
+
+		MonoQueue mq(k);
+
+		int i = 0;
+		while (i < k) {
+			mq.add(i++);
+		}
+
+        for (; i < INPUT_SIZE; i++){
+			monoqueue_outputs.push_back(mq.min());
+			mq.add(i);
+        }
+		monoqueue_outputs.push_back(mq.min());
+	}
+}
+BENCHMARK(BM_monoqueue)->RangeMultiplier(2)->Range(1<<2, 1<<6);
+
+static void BM_monoqueue2(benchmark::State& state) {
+    for(auto _ : state) {
+        state.PauseTiming();
+        monoqueue_outputs2.clear();
+		int k = state.range(0);
+        state.ResumeTiming();
+
+		MonoQueue2 mq(k);
+
+		int i = 0;
+		while (i < k) {
+			mq.add(i++);
+		}
+
+        for (; i < INPUT_SIZE; i++){
+			monoqueue_outputs2.push_back(mq.min());
+			mq.add(i);
+        }
+		monoqueue_outputs2.push_back(mq.min());
+	}
+}
+BENCHMARK(BM_monoqueue2)->RangeMultiplier(2)->Range(1<<2, 1<<6);
+
+
+
+
+
 int main(int argc, char** argv)
 {
     setupInput();
@@ -141,7 +315,9 @@ int main(int argc, char** argv)
     benchmark::RunSpecifiedBenchmarks();
     // sanity check
     // if it's correct for 1 window size, it's probably correct for all
-    assert(st_outputs == mset_outputs);
+    //assert(st_outputs == mset_outputs);
     assert(st_outputs == naive_outputs);
+    assert(st_outputs == monoqueue_outputs);
+    assert(st_outputs == monoqueue_outputs2);
     std::cout << "Passed Asserts!" << std::endl;
 }
