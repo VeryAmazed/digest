b8f4ce221324e195406c7f54f8bfc4fb00bbe524
ishmeal <me@ishme.al>
2024-01-04 07:03:33 -0500

    avx for posterity

--- tests/seg_tree/2.cpp
+++ tests/seg_tree/2.cpp
@@ -11,7 +11,6 @@ const int INPUT_SIZE = 1e7;
 std::array<uint32_t,2*INPUT_SIZE> hashes;
 
 std::map<int,std::array<uint32_t,INPUT_SIZE>> st_outputs;
-std::map<int,std::array<uint32_t,INPUT_SIZE>> st_outputs_avx;
 std::map<int,std::array<uint32_t,INPUT_SIZE>> st_outputs_normal;
 std::map<int,std::array<uint32_t,INPUT_SIZE>> mset_outputs;
 std::map<int,std::array<uint32_t,INPUT_SIZE>> naive_outputs;
@@ -85,15 +84,17 @@ static void BM_monoqueue(benchmark::State& state) {
 	}
 }
 
-template<int k>
+template<int K=1024>
 class SegmentTree {
 	int i = 0;
-	uint64_t segtree[2*k];
+	const int k;
+	uint64_t segtree[2*K];
 
-	static constexpr int log2() {
-		return std::ceil(std::log2(k));
+	constexpr int log2() {
+		return std::ceil(std::log2(K));
 	}
 public:
+	SegmentTree(int k) : k(k) {}
 	void set(uint32_t hash, uint32_t index) {
 		int ind = i + k;
 		if (++i == k) i = 0;
@@ -114,56 +115,7 @@ template<int k>
 static void BM_segtree(benchmark::State& state) {
 	auto &out = st_outputs[k];
     for(auto _ : state) {
-		SegmentTree<k> st;
-
-		for (int i = 0; i < k; i++){
-			st.set(hashes[i], i);
-		}
-		for (int i = 0; i < INPUT_SIZE; i++){
-			out[i] = st.max();
-			st.set(hashes[i+k], i+k);
-		}
-	}
-}
-
-#include <immintrin.h>
-
-template<int k>
-class SegmentTreeAvx {
-	int i = 0;
-
-	static constexpr int k512() {
-		return (k+7)/8;
-	}
-	__m512i segtree[k512() * 2] = {}; // 8 packed
-
-	static constexpr int log2() {
-		return std::ceil(std::log2(k512()));
-	}
-public:
-	void set(uint32_t hash, uint32_t index) {
-		int ind = (i>>3) + k512();
-		if (++i == k) i = 0;
-
-		// segtree[ind] = _mm512_mask_set1_epi64(segtree[ind], 1<<(i&0b111), (uint64_t)hash << 32 | index);
-		*((uint64_t *)(segtree + ind) + (i&0b111)) = (uint64_t)hash << 32 | index;
-		for (int rep = 0; rep < log2(); rep++) {
-			segtree[ind >> 1] = _mm512_max_epu64(segtree[ind], segtree[ind ^ 1]);
-			ind >>= 1;
-		}
-	}
-
-	// calling this repeatedly without calling set is wasted computation
-	uint32_t max() {
-		return _mm512_reduce_max_epu64(segtree[1]);
-	}
-};
-
-template<int k>
-static void BM_segtree_avx(benchmark::State& state) {
-	auto &out = st_outputs_avx[k];
-    for(auto _ : state) {
-		SegmentTreeAvx<k> st;
+		SegmentTree<k> st(k);
 
 		for (int i = 0; i < k; i++){
 			st.set(hashes[i], i);
@@ -180,6 +132,9 @@ class Segtree_normal {
 	const int k;
 	std::vector<uint64_t> segtree;
 
+	// constexpr int log2() {
+	// 	return std::ceil(std::log2(k));
+	// }
 public:
 	Segtree_normal(int k) : k(k), segtree(2*k) {}
 
@@ -377,21 +332,20 @@ static void BM_naive2(benchmark::State& state){
 	BENCHMARK_TEMPLATE(name, 8); \
 	BENCHMARK_TEMPLATE(name, 12); \
 	BENCHMARK_TEMPLATE(name, 16); \
-	// BENCHMARK_TEMPLATE(name, 24); \
-	// BENCHMARK_TEMPLATE(name, 32); \
-	// BENCHMARK_TEMPLATE(name, 48); \
-	// BENCHMARK_TEMPLATE(name, 64); \
-	// BENCHMARK_TEMPLATE(name, 96); \
-	// BENCHMARK_TEMPLATE(name, 128); \
-	// BENCHMARK_TEMPLATE(name, 256); \
-	// BENCHMARK_TEMPLATE(name, 512); \
-	// BENCHMARK_TEMPLATE(name, 1024); \
+	BENCHMARK_TEMPLATE(name, 24); \
+	BENCHMARK_TEMPLATE(name, 32); \
+	BENCHMARK_TEMPLATE(name, 48); \
+	BENCHMARK_TEMPLATE(name, 64); \
+	BENCHMARK_TEMPLATE(name, 96); \
+	BENCHMARK_TEMPLATE(name, 128); \
+	BENCHMARK_TEMPLATE(name, 256); \
+	BENCHMARK_TEMPLATE(name, 512); \
+	BENCHMARK_TEMPLATE(name, 1024); \
 
 test(BM_segtree);
-test(BM_segtree_avx);
-// test(BM_segtree_normal);
-// test(BM_naive);
-// test(BM_naive2);
+test(BM_segtree_normal);
+test(BM_naive);
+test(BM_naive2);
 
 // test(BM_monoqueue);
 // test(BM_mset);
@@ -403,9 +357,8 @@ int main(int argc, char** argv)
     benchmark::RunSpecifiedBenchmarks();
 
     // sanity check
-    assert(st_outputs == st_outputs_avx);
-    // assert(st_outputs == naive_outputs);
-    // assert(st_outputs == naive_outputs2);
+    assert(st_outputs == naive_outputs);
+    assert(st_outputs == naive_outputs2);
     assert(st_outputs == st_outputs_normal);
     // assert(st_outputs == monoqueue_outputs);
     // assert(st_outputs == mset_outputs);
