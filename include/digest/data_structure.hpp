6a1f4f01053b8b2a2becd47dd3017ba607579254
ishmeal <me@ishme.al>
2024-01-10 07:39:38 -0500

    data structure

---
+++ include/digest/data_structure.hpp
@@ -0,0 +1,206 @@
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <set>
+#include <vector>
+#include <utility>
+#include <stdint.h>
+
+// requirement on all data_structures
+// constructor which accepts uint32_t
+// uint32_t set(uint32_t index, uint32_t hash) // returns new minimum
+
+template<uint32_t k>
+struct MonoQueue {
+ 	int head = 0, tail = 0;
+	// one extra slot so empty() works when full
+	// {hash, index, time}
+	std::array<std::array<uint32_t,k+1>,3> queue;
+	uint32_t time = 0;
+
+ 	bool empty() {
+ 		return head == tail;
+ 	}
+
+	MonoQueue(uint32_t) {
+		queue[2].fill(0); // necessary to avoid 1 in a billion collision
+		queue[0].fill(0); // gets rid of warning
+	}
+
+ 	uint32_t insert(uint32_t index, uint32_t hash) {
+ 		if (queue[2][head] == time - k) {
+			if (++head == k+1) {
+				head = 0;
+			}
+ 		}
+
+ 		while (not empty() and queue[0][tail == 0 ? k : tail - 1] >= hash) {
+			if (--tail == -1) {
+				tail = k;
+			}
+ 		}
+
+		queue[0][tail] = hash;
+		queue[1][tail] = index;
+		queue[2][tail] = time++;
+
+ 		if (++tail == k+1) tail = 0;
+
+ 		return queue[1][head];
+ 	}
+};
+
+
+// Based on a template taken from USACO.guide and then modified by me (for competitive programming), and now modified again (for this)
+// https://usaco.guide/gold/PURS?lang=cpp
+// https://codeforces.com/blog/entry/18051 (USACO.guide was probably heavily inspired by this)
+/** A data structure that can answer point update & range minimum queries. */
+template<int k>
+struct SegmentTree {
+	int i = 0;
+	uint64_t segtree[2*k];
+
+	constexpr int log2() {
+		return std::ceil(std::log2(k));
+	}
+
+	SegmentTree(uint32_t) {}
+
+	uint32_t insert(uint32_t index, uint32_t hash) {
+		int ind = i + k;
+		if (++i == k) i = 0;
+
+		// negate so we can use max so that ties are broken by rightmost
+		segtree[ind] = (uint64_t)~hash << 32 | index;
+		for (int rep = 0; rep < log2(); rep++) {
+			segtree[ind >> 1] = std::max(segtree[ind], segtree[ind ^ 1]);
+			ind >>= 1;
+		}
+
+		return segtree[1];
+	}
+};
+
+template<uint32_t k>
+struct Set {
+	std::set<uint64_t> mset;
+	std::array<std::set<uint64_t>::iterator,k> vec;
+	int i = 0;
+
+	Set(uint32_t) {
+		// edge case where hash == all 1's can cause a set collision.
+		for (unsigned i = 0; i < k; i++) {
+			vec[i] = mset.emplace(i).first;
+		}
+	}
+
+	uint32_t insert(uint32_t index, uint32_t hash) {
+		mset.erase(vec[i]);
+
+		vec[i] = mset.emplace((uint64_t)~hash << 32 | index).first;
+		if (++i == k) i = 0;
+
+		return *mset.rbegin();
+	}
+};
+
+template<uint32_t k>
+struct Naive {
+	std::array<uint64_t,k> arr;
+	int i = 0;
+
+	Naive(uint32_t) {};
+
+	uint32_t insert(uint32_t index, uint32_t hash) {
+		arr[i] = (uint64_t)~hash << 32 | index;
+		if (++i == k) i = 0;
+
+		// get min
+		int i = k-1;
+		for (int j = k-2; j >= 0; j--) {
+			if (arr[j] > arr[i]) {
+				i = j;
+			}
+		}
+		return arr[i];
+	}
+};
+
+template<uint32_t k>
+struct Naive2 {
+	int i = 0;
+	int last = 0;
+	std::vector<uint64_t> arr = std::vector<uint64_t>(k);
+
+	Naive2(uint32_t) {};
+
+	uint32_t insert(uint32_t index, uint32_t hash) {
+		// flip the hash bits so we can take the maximum
+		arr[i] = (uint64_t)~hash << 32 | index;
+
+		if (arr[i] > arr[last]) {
+			last = i;
+		} else if (last == i) {
+			for (unsigned j = 0; j < k; j++) {
+				if (arr[j] > arr[last]) {
+					last = j;
+				}
+			}
+		}
+
+		if (++i == k) i = 0;
+
+		return arr[last];
+	}
+};
+
+struct Adaptive {
+	const uint32_t k;
+	uint32_t i = 0, last = 0;
+	std::vector<uint64_t> arr;
+
+	Adaptive(uint32_t k) : k(k), arr(k) {}
+
+	uint32_t naive(uint32_t index, uint32_t hash) {
+		arr[i] = (uint64_t)~hash << 32 | index;
+		if (++i == k) i = 0;
+
+		// get min
+		int i = k-1;
+		for (int j = k-2; j >= 0; j--) {
+			if (arr[j] > arr[i]) {
+				i = j;
+			}
+		}
+		return arr[i];
+	}
+
+	uint32_t naive2(uint32_t index, uint32_t hash) {
+		// flip the hash bits so we can take the maximum
+		arr[i] = (uint64_t)~hash << 32 | index;
+
+		if (arr[i] > arr[last]) {
+			last = i;
+		} else if (last == i) {
+			for (unsigned j = 0; j < k; j++) {
+				if (arr[j] > arr[last]) {
+					last = j;
+				}
+			}
+		}
+
+		if (++i == k) i = 0;
+
+		return arr[last];
+	}
+
+	uint32_t insert(uint32_t index, uint32_t hash) {
+		if (k < 16) {
+			return naive(index, hash);
+		}
+		return naive2(index, hash);
+	}
+};
+
---
+++ tests/data_structure/bench_ds.cpp
@@ -0,0 +1,103 @@
+#include "digest/data_structure.hpp"
+#include <benchmark/benchmark.h>
+
+#include <cstdint>
+#include <iostream>
+#include <random>
+
+// segtee wins at 12
+// naive2 wins at 17
+
+const int INPUT_SIZE = 1e7;
+std::array<uint32_t,2*INPUT_SIZE> hashes;
+
+std::map<int,std::map<int,std::array<uint32_t, INPUT_SIZE>>> all;
+
+void setupInput(){
+	std::random_device rd; // seed
+	std::mt19937 gen(rd()); // generator
+	std::uniform_int_distribution<uint32_t> distrib(0,UINT32_MAX); // [0, 2**32]
+	for (uint32_t &h : hashes) {
+        h = distrib(gen);
+    }
+
+	// edge test for ties
+	// for (int i = 0; i < 2*INPUT_SIZE; i++) {
+	// 	hashes[i] = hashes[0];
+	// }
+}
+
+
+template<int k, class T, int out>
+static void BM(benchmark::State& state){
+	auto &temp = all[out][k];
+    for(auto _ : state) {
+		T ds(k);
+		for (int i = 0; i < k-1; i++) {
+			ds.insert(i, hashes[i]);
+		}
+        for (int i = 0; i < INPUT_SIZE; i++) {
+            temp[i] = ds.insert(i+k-1, hashes[i+k-1]);
+        }
+		benchmark::ClobberMemory();
+	}
+}
+
+#define test(name, out) \
+	BENCHMARK_TEMPLATE(BM, 2, name<2>, out); \
+	BENCHMARK_TEMPLATE(BM, 3, name<3>, out); \
+	BENCHMARK_TEMPLATE(BM, 4, name<4>, out); \
+	// BENCHMARK_TEMPLATE(BM, 5, name<5>, out); \
+	// BENCHMARK_TEMPLATE(BM, 8, name<8>, out); \
+	// BENCHMARK_TEMPLATE(BM, 9, name<9>, out); \
+	// BENCHMARK_TEMPLATE(BM, 12, name<12>, out); \
+	// BENCHMARK_TEMPLATE(BM, 16, name<16>, out); \
+	// BENCHMARK_TEMPLATE(BM, 17, name<17>, out); \
+	// BENCHMARK_TEMPLATE(BM, 32, name<32>, out); \
+	// BENCHMARK_TEMPLATE(BM, 33, name<33>, out); \
+	// BENCHMARK_TEMPLATE(BM, 64, name<64>, out); \
+	// BENCHMARK_TEMPLATE(BM, 96, name<96>, out); \
+	// BENCHMARK_TEMPLATE(BM, 128, name<128>, out); \
+	// BENCHMARK_TEMPLATE(BM, 256, name<256>, out); \
+	// BENCHMARK_TEMPLATE(BM, 512, name<512>, out); \
+	// BENCHMARK_TEMPLATE(BM, 1024, name<1024>, out); \
+
+#define test2(name, out) \
+	BENCHMARK_TEMPLATE(BM, 2, name, out); \
+	BENCHMARK_TEMPLATE(BM, 3, name, out); \
+	BENCHMARK_TEMPLATE(BM, 4, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 5, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 8, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 9, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 12, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 16, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 17, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 32, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 33, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 64, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 96, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 128, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 256, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 512, name, out); \
+	// BENCHMARK_TEMPLATE(BM, 1024, name, out); \
+
+test(Naive, 0);
+test(Naive2, 1);
+test(MonoQueue, 2);
+test(SegmentTree, 3);
+test(Set, 4);
+test2(Adaptive, 5);
+
+int main(int argc, char** argv)
+{
+    setupInput();
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+
+    // sanity check
+	for (auto &[_, m] : all) {
+		assert(m == all[0]);
+	}
+
+    std::cout << "Passed Asserts!" << std::endl;
+}
