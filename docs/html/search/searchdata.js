9192c3aeeeaf713797db338572bcb207f5b924e4
ishmeal <theoleehusky@gmail.com>
2024-03-25 19:53:46 +0000

    doxy moved to docs folder

--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,21 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "abdgimnrsw",
-  1: "abdmnsw",
-  2: "adgimnrsw"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "functions"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Functions"
 };
 
