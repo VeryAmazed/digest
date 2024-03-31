9c3e564b98a0734572e4ef096805b87c875944f1
VeryAmazed <alan.h.zheng@outlook.com>
2024-03-31 01:15:39 -0400

    enabled extract_local_methods to YES in doxyfile. We'll see if this will then display the functions in thread_out.hpp. Documented stuff in digester.hpp. Also looking if @internal does hide stuff like I hoped it would

--- include/digest/digester.hpp
+++ include/digest/digester.hpp
@@ -7,12 +7,21 @@
 
 namespace digest {
 
+/**
+ * @brief Exception thrown when initializing a Digister with k (kmer size) < 4 and start (starting index) < len (length of sequence). 
+ * 
+ * 
+ */
 class BadConstructionException : public std::exception {
   const char *what() const throw() {
     return "k must be greater than 3, start must be less than len";
   }
 };
 
+/**
+ * @brief Exception thrown when append_seq() is called before all kmers/large windows in the current sequence have been iterated over
+ * 
+ */
 class NotRolledTillEndException : public std::exception {
   const char *what() const throw() {
     return "Iterator must be at the end of the current sequence before "
@@ -20,25 +29,39 @@ class NotRolledTillEndException : public std::exception {
   }
 };
 
+/**
+ * @brief Specify whether or not the hash value we are minimizing is the canonical hash, forward hash, or reverse hash
+ * 
+ */
 enum class MinimizedHashType { CANON, FORWARD, REVERSE };
 
+/**
+ * @brief Digest does not not consider kmers with non-ACTG characters. Non-ACTG characters in the sequence can be handled in one of two ways.
+ * The WRITEOVER policy specifies that any non-ACTG character is simply replaced with an A. The SKIPOVER policy skips over any kmers with a non-ACTG
+ * character. For example, if you have k = 4 and your sequence is ACTGNNACTGAC, then the only kmers that would be considered would be the ACTG starting at
+ * index 0, the ACTG starting at index 6, CTGA at index 7, and TGAC at index 8. Then if you had a large window of 4 (kmers), then the smallest would be picked
+ * from one of those 4.
+ * 
+ */
 enum class BadCharPolicy { WRITEOVER, SKIPOVER };
 
-// Only supports characters in DNA and N, upper or lower case
+/**
+ * @brief an abstract class for Digester objects.  
+ * 
+ * @tparam a BadCharPolicy enum value. The policy to adopt when handling non-ACTG characters. 
+ * 
+ */
 template <BadCharPolicy P> class Digester {
 public:
   /**
-   * @param seq char pointer poitning to the c-string of DNA sequence to be
-   * hashed.
+   * @param seq const char pointer pointing to the c-string of DNA sequence to be hashed.
    * @param len length of seq.
-   * @param k k-mer size.
+   * @param k kmer size.
    * @param start 0-indexed position in seq to start hashing from.
-   * @param minimized_h hash to be minimized, 0 for canoncial, 1 for forward, 2
-   * for reverse
+   * @param minimized_h whether we are minimizing the canonical, forward, or reverse hash
    *
    * @throws BadConstructionException Thrown if k is less than 4,
-   *      or if the starting position is after the end of the string
-   *      or if minimized_h is greater than 2
+   * or if the starting position is after the end of the string
    */
   Digester(const char *seq, size_t len, unsigned k, size_t start = 0,
            MinimizedHashType minimized_h = MinimizedHashType::CANON)
@@ -51,14 +74,13 @@ public:
   }
 
   /**
-   * @param seq reference to std string of DNA sequence to be hashed.
-   * @param k k-mer size.
+   * @param seq const string of the DNA sequence to be hashed.
+   * @param k kmer size.
    * @param start 0-indexed position in seq to start hashing from.
-   * @param minimized_h hash to be minimized, 0 for canoncial, 1 for forward, 2
-   * for reverse
+   * @param minimized_h whether we are minimizing the canonical, forward, or reverse hash
    *
    * @throws BadConstructionException Thrown if k is less than 4,
-   *      or if the starting position is after the end of the string
+   * or if the starting position is after the end of the string
    */
   Digester(const std::string &seq, unsigned k, size_t start = 0,
            MinimizedHashType minimized_h = MinimizedHashType::CANON)
@@ -73,14 +95,19 @@ public:
    */
   bool get_is_valid_hash() { return is_valid_hash; }
 
+  /**
+   * @return unsigned, the value of k (kmer size)
+   */
   unsigned get_k() { return k; }
 
+  /**
+   * @return size_t, the length of the sequence
+   */
   size_t get_len() { return len; }
 
   /**
-   * @brief moves the internal pointer to the next valid k-mer, skipping over
-   * any k-mers that have contain a non ACTG character, and returns hash for
-   * that k-mer Time Complexity: O(1)
+   * @brief moves the internal pointer to the next valid k-mer. <br>
+   * Time Complexity: O(1)
    *
    * @return bool, true if we were able generate a valid hash, false otherwise
    */
@@ -93,21 +120,21 @@ public:
   };
 
   /**
-   * @brief returns the positions, as defined by get_pos(), of minimizers up to
+   * @brief gets the positions, as defined by get_pos(), of minimizers up to
    * the amount specified
    *
    * @param amount number of minimizers you want to generate
-   * @param vec a reference to a vector of size_t's, the positions returned will
+   * @param vec a reference to a vector of uint32_t, the positions returned will
    * go there
    */
   virtual void roll_minimizer(unsigned amount, std::vector<uint32_t> &vec) = 0;
 
   /**
-   * @brief returns the positions (pair.first), as defined by get_pos(), and the
+   * @brief gets the positions (pair.first), as defined by get_pos(), and the
    * hashes (pair.second) of minimizers up to the amount specified
    *
    * @param amount number of minimizers you want to generate
-   * @param vec a reference to a vector of size_t's, the positions returned will
+   * @param vec a reference to a vector of a pair of uint32_t, the positions and hashes returned will
    * go there
    */
   virtual void
@@ -116,25 +143,34 @@ public:
 
   /**
    * @return current index of the first character of the current kmer that has
-   * been hashed strings that have been appended onto each other count as 1 big
-   * string, i.e. if you first had a string of length 10 and then appended
-   * another string of length 20, and the index of the first character of the
-   * current k-mer is at index 4, 0-indexed, in the second string, then it will
+   * been hashed. Sequences that have been appended onto each other count as 1 big
+   * sequence, i.e. if you first had a sequence of length 10 and then appended
+   * another sequence of length 20, and the index of the first character of the
+   * current k-mer is at index 4, 0-indexed, in the second sequence, then it will
    * return 14
    */
   size_t get_pos() { return offset + start - c_outs.size(); }
 
+  /**
+   * @return uint64_t, the canonical hash of the kmer that was rolled over when roll_one was last called (roll_minimizer() calls roll_one() internally). 
+   */
   uint64_t get_chash() { return chash; }
 
+  /**
+   * @return uint64_t, the forward hash of the kmer that was rolled over when roll_one was last called (roll_minimizer() calls roll_one() internally). 
+   */
   uint64_t get_fhash() { return fhash; }
 
+  /**
+   * @return uint64_t, the reverse hash of the kmer that was rolled over when roll_one was last called (roll_minimizer() calls roll_one() internally). 
+   */
   uint64_t get_rhash() { return rhash; }
 
   /**
-   * @brief replaces the current sequence with the new one, it's like starting
-   * over with a completely new string
+   * @brief replaces the current sequence with the new one. It's like starting
+   * over with a completely new seqeunce
    *
-   * @param seq char pointer to new sequence to be hashed
+   * @param seq const char pointer to new sequence to be hashed
    * @param len length of the new sequence
    * @param start position in new sequence to start from
    *
@@ -155,10 +191,10 @@ public:
   }
 
   /**
-   * @brief replaces the current sequence with the new one, it's like starting
-   * over with a completely new string
+   * @brief replaces the current sequence with the new one. It's like starting
+   * over with a completely new sequence
    *
-   * @param seq std string reference to the new sequence to be hashed
+   * @param seq const std string reference to the new sequence to be hashed
    * @param start position in new sequence to start from
    *
    * @throws BadConstructionException thrown if the starting position is greater
@@ -170,16 +206,16 @@ public:
 
   /**
    * @brief simulates the appending of a new sequence to the end of the old
-   * sequence The old string will no longer be stored, but the rolling hashes
-   * will be able to preceed as if the strings were appended Can only be called
-   * when you've reached the end of the current string i.e. if you're current
+   * sequence. The old sequence will no longer be stored, but the rolling hashes
+   * will be able to preceed as if the sequences were appended. Can only be called
+   * when you've reached the end of the current sequence i.e. if you're current
    * sequence is ACTGAC, and you have reached the end of this sequence, and you
    * call append_seq with the sequence CCGGCCGG, then the minimizers you will
    * get after calling append_seq plus the minimizers you got from going through
    * ACTGAC, will be equivalent to the minimizers you would have gotten from
    * rolling across ACTGACCCGGCCGG
    *
-   * @param seq C string of DNA sequence to be appended
+   * @param seq const C string of DNA sequence to be appended
    * @param len length of the sequence
    *
    * @throws NotRolledTillEndException Thrown when the internal iterator is not
@@ -195,16 +231,16 @@ public:
 
   /**
    * @brief simulates the appending of a new sequence to the end of the old
-   * sequence The old string will no longer be stored, but the rolling hashes
-   * will be able to preceed as if the strings were appended Can only be called
-   * when you've reached the end of the current string i.e. if you're current
+   * sequence. The old sequence will no longer be stored, but the rolling hashes
+   * will be able to preceed as if the sequences were appended. Can only be called
+   * when you've reached the end of the current sequence i.e. if you're current
    * sequence is ACTGAC, and you have reached the end of this sequence, and you
    * call append_seq with the sequence CCGGCCGG, then the minimizers you will
    * get after calling append_seq plus the minimizers you got from going through
    * ACTGAC, will be equivalent to the minimizers you would have gotten from
    * rolling across ACTGACCCGGCCGG
    *
-   * @param seq std string of DNA sequence to be appended
+   * @param seq const std string of DNA sequence to be appended
    *
    * @throws NotRolledTillEndException Thrown when the internal iterator is not
    * at the end of the current sequence
@@ -229,8 +265,12 @@ public:
   const char *get_sequence() { return seq; }
 
 protected:
-  // 0x41 = 'A', 0x43 = 'C', 0x47 = 'G' 0x54 = 'T'
-  // 0x61 = 'a', 0x63 = 'c', 0x67 = 'g' 0x74 = 't'
+  /**
+   * @internal
+   * 
+   * 0x41 = 'A', 0x43 = 'C', 0x47 = 'G' 0x54 = 'T'
+   * 0x61 = 'a', 0x63 = 'c', 0x67 = 'g' 0x74 = 't'
+   */
   std::array<bool, 256> actg{
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // NOLINT
@@ -251,15 +291,19 @@ protected:
   };
 
   /**
+   * @internal
    * Helper function
    *
    * @param in char to be checked
+   * 
    * @return bool, true if in is an upper or lowercase ACTG character, false
    * otherwise
    */
   bool is_ACTG(char in) { return actg[in]; }
 
   /**
+   * @internal
+   * 
    * @brief Helper function that initializes the hash values at the first valid
    * k-mer at or after start Sets is_valid_hash to be equal to its return value
    *
