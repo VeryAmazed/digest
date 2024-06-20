567bb4f79cc537652d16c7fc3fe766e57928c916
Vikram Shivakumar <vshivak1@jhu.edu>
2024-06-20 18:45:20 -0400

    added include_hash flag

--- pybind/bindings.cpp
+++ pybind/bindings.cpp
@@ -7,9 +7,9 @@ namespace py = pybind11;
 PYBIND11_MODULE(Digest, m) {
     m.doc() = "bindings for Digest";
     m.def("window_minimizer", &window_minimizer, "A function that runs window minimizer digestion",
-            py::arg("seq"), py::arg("k") = 31, py::arg("w") = 11);
+            py::arg("seq"), py::arg("k") = 31, py::arg("w") = 11, py::arg("include_hash") = false);
     m.def("modimizer", &modimizer, "A function that runs mod-minimizer digestion",
-            py::arg("seq"), py::arg("k") = 31, py::arg("mod") = 100);
+            py::arg("seq"), py::arg("k") = 31, py::arg("mod") = 100, py::arg("include_hash") = false);
     m.def("syncmer", &syncmer, "A function that runs syncmer digestion",
-            py::arg("seq"), py::arg("k") = 31, py::arg("w") = 11);
+            py::arg("seq"), py::arg("k") = 31, py::arg("w") = 11, py::arg("include_hash") = false);
 }
