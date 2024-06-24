77da47c373c8f830fd983fc016e6eb452128da74
Vikram Shivakumar <vshivak1@jhu.edu>
2024-06-24 17:38:27 -0400

    change syncmer policy to writeover in bindings

--- pybind/digest_utils.hpp
+++ pybind/digest_utils.hpp
@@ -36,7 +36,7 @@ std::variant<std::vector<uint32_t>, std::vector<std::pair<uint32_t, uint32_t>>> 
 
 std::variant<std::vector<uint32_t>, std::vector<std::pair<uint32_t, uint32_t>>> syncmer (
         const std::string &seq, unsigned k, unsigned large_window, bool include_hash=false) {
-    digest::Syncmer<digest::BadCharPolicy::SKIPOVER, digest::ds::Adaptive> digester (seq, k, large_window);
+    digest::Syncmer<digest::BadCharPolicy::WRITEOVER, digest::ds::Adaptive> digester (seq, k, large_window);
     if (include_hash) {
         std::vector<std::pair<uint32_t, uint32_t>> output;
         digester.roll_minimizer(seq.length(), output);
